6c3e802e993ab614aefd08a3bacc85d7af05c7e1
vihl <vihl@sandinh.net>
2022-10-22 11:07:45 +0700

    init project

---
+++ Assets/Scripts/BaseStructures/ProduceService.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProduceService : MonoBehaviour
+{
+    public MainPotData mainPot;
+    public float stepDelay = 0.5f;
+
+}
+
+
+public class MainPotData
+{
+    public int recordTime;
+    public int mainPotSize;
+    public int mainPotSpeed;
+    public int mainPotCapacity;
+}
+
+
